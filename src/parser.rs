f2cf0310d0f58727b62a0032f0348a2e11e229b8
Carlos Carral C <carloscarral13@gmail.com>
2022-03-09 14:13:14 -0600

    refactor: renombrar Interprete -> Parser

--- src/parser.rs
+++ src/parser.rs
@@ -2,7 +2,7 @@ pub use pest_derive::Parser;
 
 #[derive(Parser)]
 #[grammar = "gramatica.pest"]
-pub struct Interprete;
+pub struct Parser;
 
 #[cfg(test)]
 mod test {
@@ -11,20 +11,20 @@ mod test {
 
     #[test]
     pub fn test_decl() {
-        let decl_parse_result = Interprete::parse(Rule::decl, "var x=1;");
+        let decl_parse_result = Parser::parse(Rule::decl, "var x=1;");
         assert!(decl_parse_result.is_ok());
-        let decl_parse_result = Interprete::parse(Rule::decl, "var x = 1;");
+        let decl_parse_result = Parser::parse(Rule::decl, "var x = 1;");
         assert!(decl_parse_result.is_ok());
-        let decl_parse_result = Interprete::parse(Rule::expr, "var x = 1;");
+        let decl_parse_result = Parser::parse(Rule::expr, "var x = 1;");
         assert!(decl_parse_result.is_ok());
     }
 
     #[test]
     pub fn test_prog() {
-        let prog = Interprete::parse(Rule::prog, "var x = 1;\nvar wey = 4;");
+        let prog = Parser::parse(Rule::prog, "var x = 1;\nvar wey = 4;");
         assert!(prog.is_ok());
 
-        let prog = Interprete::parse(Rule::prog, "\nvar x = 1;\n\n");
+        let prog = Parser::parse(Rule::prog, "\nvar x = 1;\n\n");
         assert!(prog.is_ok());
     }
 }
