5f8b8353639efde3afc893e76b0f7956e74b505a
Carlos Carral C <carloscarral13@gmail.com>
2022-03-10 19:00:39 -0600

    feature: procesar una expresión dentro de un paréntesis

--- src/interprete.rs
+++ src/interprete.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 
+use crate::error::ErrorInterprete;
 use crate::parser::*;
 use pest::error::Error;
 use pest::iterators::{Pair, Pairs};
@@ -8,6 +9,7 @@ use pest::Parser;
 pub struct Interprete<'a> {
     pairs: Pairs<'a, Rule>,
     vars: HashMap<String, isize>,
+    expr_stack: Vec<isize>,
 }
 
 impl<'a> Interprete<'a> {
@@ -17,30 +19,50 @@ impl<'a> Interprete<'a> {
             println!("{pair}");
         }
         let vars = HashMap::new();
-        Ok(Self { pairs, vars })
+        Ok(Self {
+            pairs,
+            vars,
+            expr_stack: Vec::new(),
+        })
     }
 
     pub fn get_var_value(&self, varname: &str) -> Option<&isize> {
         self.vars.get(&varname.to_string())
     }
 
-    fn parse_node(&mut self, pair: Pair<Rule>) {
+    fn parse_node(&mut self, pair: Pair<Rule>) -> Result<(), ErrorInterprete> {
         match pair.as_rule() {
             Rule::inst => {
                 let inst_inner = pair.into_inner().next().unwrap();
-                self.parse_node(inst_inner);
+                self.parse_node(inst_inner)?;
             }
             Rule::decl => {
                 let mut decl_pairs = pair.into_inner();
-
                 let var_name = decl_pairs.next().unwrap().as_str();
-                let valor: isize = decl_pairs.next().unwrap().as_str().parse().unwrap();
-
+                let expr = decl_pairs.next().unwrap();
+                self.parse_node(expr)?;
+                let valor = self.expr_stack.pop().unwrap();
                 self.vars.insert(var_name.into(), valor);
+            }
+            Rule::expr => {
+                let expr_inner = pair.into_inner().next().unwrap();
+                // dbg!(&expr_inner);
+                self.parse_node(expr_inner)?;
+            }
+            Rule::expr_par => {
+                let expr_par_inner = pair.into_inner().next().unwrap();
+                self.parse_node(expr_par_inner)?;
+            }
+            Rule::int => {
+                dbg!(&pair);
+                let valor: isize = pair.as_str().parse().unwrap();
+                self.expr_stack.push(valor);
             }
 
             _ => unreachable!(),
         }
+
+        Ok(())
     }
 
     pub fn step_inst(&mut self) {
@@ -57,7 +79,7 @@ mod test {
 
     #[test]
     pub fn test_var_decl() {
-        let mut interprete = Interprete::new("var x = 1;var y =-33;").unwrap();
+        let mut interprete = Interprete::new("var x =( 1 );var y = (-33) ;").unwrap();
         interprete.step_inst();
         assert_eq!(interprete.get_var_value("x"), Some(&1));
         assert_eq!(interprete.get_var_value("y"), None);
