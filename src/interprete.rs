a52081d0640d100a00eca529196ed6c1c1fe39ea
Carlos Carral C <carloscarral13@gmail.com>
2022-04-11 09:29:54 -0500

    refactor: mover los tests a una carpeta aparte.

--- src/interprete.rs
+++ src/interprete.rs
@@ -134,7 +134,7 @@ impl<'a> Interpreter<'a> {
     }
 }
 
-fn eval(expr: Pairs<Rule>, scope: &Scope) -> Result<isize, ErrorInterprete> {
+pub fn eval(expr: Pairs<Rule>, scope: &Scope) -> Result<isize, ErrorInterprete> {
     let climber = PrecClimber::new(vec![
         Operator::new(Rule::suma, Assoc::Left) | Operator::new(Rule::resta, Assoc::Left),
         Operator::new(Rule::mult, Assoc::Left) | Operator::new(Rule::div, Assoc::Left),
@@ -177,7 +177,7 @@ fn eval(expr: Pairs<Rule>, scope: &Scope) -> Result<isize, ErrorInterprete> {
     climber.climb(expr, primary, infix)
 }
 
-fn eval_logic(expr: Pairs<Rule>, scope: &Scope) -> Result<bool, ErrorInterprete> {
+pub fn eval_logic(expr: Pairs<Rule>, scope: &Scope) -> Result<bool, ErrorInterprete> {
     let climber = PrecClimber::new(vec![
         Operator::new(Rule::men, Assoc::Left)
             | Operator::new(Rule::may, Assoc::Left)
@@ -240,105 +240,4 @@ fn eval_logic(expr: Pairs<Rule>, scope: &Scope) -> Result<bool, ErrorInterprete>
     };
 
     climber.climb(expr, primary, infix)
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    pub fn test_expr() {
-        let mut interprete = Interpreter::new(
-            "var x = 1 + 2;var y = 1 - 2 ;var z = 4 * 2; var w = 4/(2*2); var a = w + 10; var b = n + 1;",
-        )
-        .unwrap();
-        interprete.step_inst().unwrap();
-        assert_eq!(interprete.get_var_value("x"), Some(3));
-        interprete.step_inst().unwrap();
-        assert_eq!(interprete.get_var_value("y"), Some(-1));
-        interprete.step_inst().unwrap();
-        assert_eq!(interprete.get_var_value("z"), Some(8));
-        interprete.step_inst().unwrap();
-        assert_eq!(interprete.get_var_value("w"), Some(1));
-        interprete.step_inst().unwrap();
-        assert_eq!(interprete.get_var_value("a"), Some(11));
-        let res = interprete.step_inst();
-        assert!(res.is_err());
-    }
-
-    #[test]
-    pub fn test_expr_logic() {
-        let eval_expr = |expr: &str, scope: &Scope| {
-            let pairs = ParserTanques::parse(Rule::expr_logic, expr).unwrap();
-            eval_logic(pairs, &scope).unwrap()
-        };
-
-        let mut scope = Scope::new();
-
-        let val = eval_expr("2 == 1", &scope);
-        assert!(!val);
-
-        let val = eval_expr("1 < 0", &scope);
-        assert!(!val);
-
-        let val = eval_expr("4 == 4", &scope);
-        assert!(val);
-
-        let val = eval_expr("4 != 3", &scope);
-        assert!(val);
-
-        let val = eval_expr("1 < 2 && 6 == 7", &scope);
-        assert!(!val);
-
-        let val = eval_expr("1 < 2 && 6 == 6 && 7 != 7", &scope);
-        assert!(!val);
-
-        let val = eval_expr("1 < 2 || 6 == 6 && 7 != 7", &scope);
-        assert!(val);
-
-        scope.set_var("x", 8);
-        let val = eval_expr("x == 8", &scope);
-        assert!(val);
-
-        let val = eval_expr("x != 8", &scope);
-        assert!(!val);
-
-        scope.set_var("y", 9);
-        let val = eval_expr("x < y", &scope);
-        assert!(val);
-
-        let val = eval_expr("x == y", &scope);
-        assert!(!val);
-    }
-
-    #[test]
-    fn test_asig() {
-        let mut interprete = Interpreter::new("var x = 1;x = x + 1;").unwrap();
-        interprete.step_inst().unwrap();
-        assert_eq!(interprete.get_var_value("x"), Some(1));
-        interprete.step_inst().unwrap();
-        assert_eq!(interprete.get_var_value("x"), Some(2));
-    }
-
-    #[test]
-    fn test_si() {
-        let mut interprete =
-            Interpreter::new("var x = 1; si(x == 1){ x = x + 2; x = x + 4; } var y = 2; x = 10;")
-                .unwrap();
-        interprete.step_inst().unwrap();
-        assert_eq!(interprete.get_var_value("x"), Some(1));
-
-        interprete.step_inst().unwrap();
-        assert_eq!(interprete.get_var_value("x"), Some(3));
-
-        interprete.step_inst().unwrap();
-        assert_eq!(interprete.get_var_value("x"), Some(7));
-
-        interprete.step_inst().unwrap();
-        assert_eq!(interprete.get_var_value("y"), Some(2));
-        assert_eq!(interprete.get_var_value("x"), Some(1));
-
-        interprete.step_inst().unwrap();
-        assert_eq!(interprete.get_var_value("x"), Some(10));
-    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod error;
 mod interprete;
 mod parser;
 mod scope;
+mod test;
 fn main() {
     println!("Hello world!");
 }
